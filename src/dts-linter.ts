2e3922fba36bef07503715cd8a0b349b6602e731
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-06-27 01:14:06 +0200

    More improvments

--- src/dts-linter.ts
+++ src/dts-linter.ts
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
-import cp from "child_process";
+import cp, { execFileSync } from "child_process";
 import { ContextListItem, File } from "devicetree-language-server-types";
-import { createPatch } from "diff";
 import fs, { existsSync } from "fs";
-import path, { basename } from "path";
-import { fileURLToPath } from "url";
+import path from "path";
 import { createMessageConnection, MessageConnection } from "vscode-jsonrpc";
 import { StreamMessageReader, StreamMessageWriter } from "vscode-jsonrpc/node";
 import {
   TextDocumentItem,
   DocumentFormattingParams,
-  TextEdit,
+  Diagnostic,
+  DiagnosticSeverity,
 } from "vscode-languageserver-protocol";
-import { TextDocument } from "vscode-languageserver-textdocument";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
@@ -34,11 +32,29 @@ const argv = yargs(hideBin(process.argv))
     describe: "List of files to format",
     demandOption: true,
   })
+  .option("includes", {
+    type: "array",
+    describe: "Include List",
+  })
+  .option("bindings", {
+    type: "array",
+    describe: "Bindings paths",
+  })
   .option("logLevel", {
     type: "string",
-    choices: ["none", "verbose", "diff"],
+    choices: ["none", "verbose", "issues"],
     default: "none",
     describe: "Log level for output",
+  })
+  .option("formating", {
+    type: "boolean",
+    default: true,
+    describe: "Format files",
+  })
+  .option("diagnostics", {
+    type: "boolean",
+    default: false,
+    describe: "Report diagnostics",
   })
   .option("outFile", {
     type: "string",
@@ -50,15 +66,28 @@ const argv = yargs(hideBin(process.argv))
 
 type LogLevel = "none" | "verbose" | "diff";
 const files = argv.files as string[];
+const dtsIncludes = (argv.includes ?? []) as string[];
+const bindings = (argv.bindings ?? []) as string[];
 const logLevel = argv.logLevel as LogLevel;
+const formating = argv.formating;
+const diagnostics = argv.diagnostics;
 const outFile: string | undefined = argv.outFile;
 
-run(files, logLevel, outFile).catch((err) => {
+run(files, logLevel, dtsIncludes, bindings, outFile).catch((err) => {
   console.error("Error validating files:", err);
   process.exit(1);
 });
 
-async function run(filePaths: string[], logLevel: LogLevel, outFile?: string) {
+let i = 0;
+let total = files.length;
+
+async function run(
+  filePaths: string[],
+  logLevel: LogLevel,
+  includesPaths: string[],
+  Bindings: string[],
+  outFile?: string
+) {
   const lspPath = "./dist/server.js";
 
   const lspProcess = cp.spawn(lspPath, ["--stdio"], {
@@ -103,9 +132,9 @@ async function run(filePaths: string[], logLevel: LogLevel, outFile?: string) {
   connection.sendNotification("workspace/didChangeConfiguration", {
     settings: {
       devicetree: {
-        defaultIncludePaths: [],
+        defaultIncludePaths: includesPaths,
         defaultBindingType: "Zephyr",
-        defaultZephyrBindings: [],
+        defaultZephyrBindings: bindings,
         autoChangeContext: true,
         allowAdhocContexts: true,
         defaultLockRenameEdits: [],
@@ -127,8 +156,15 @@ async function run(filePaths: string[], logLevel: LogLevel, outFile?: string) {
   const completedPaths = new Set<string>();
   const diffs: string[] = [];
   const paths = new Set(filePaths);
-  let error = 0;
+  let formatingErrors: { file: string; context: ContextListItem }[] = [];
+  let diagnosticIssues: {
+    file: string;
+    message: string;
+    context: ContextListItem;
+  }[] = [];
+
   for (const filePath of paths) {
+    i++;
     const text = fs.readFileSync(filePath, "utf8");
 
     const textDocument: TextDocumentItem = {
@@ -143,23 +179,48 @@ async function run(filePaths: string[], logLevel: LogLevel, outFile?: string) {
     });
 
     const context = await waitForNewActiveContext(connection);
-    const files = [
-      ...flatFileTree(context.mainDtsPath),
-      ...context.overlays.flatMap(flatFileTree),
-    ];
+    const files = filePath.endsWith(".overlay")
+      ? [context.mainDtsPath.file]
+      : [
+          ...flatFileTree(context.mainDtsPath),
+          ...context.overlays.flatMap(flatFileTree),
+        ];
 
     await Promise.all(
       files.map(async (f) => {
-        if (!paths.has(f) || completedPaths.has(f)) {
+        if (completedPaths.has(f) || f.endsWith(".h") || !existsSync(f)) {
           return;
         }
 
-        const diff = await formatFile(connection, f);
-        completedPaths.add(f);
-        if (diff) {
-          error++;
-          if (outFile) {
-            diffs.push(diff);
+        if (formating) {
+          const diff = await formatFile(connection, f);
+          completedPaths.add(f);
+          if (diff) {
+            formatingErrors.push({
+              file: f,
+              context,
+            });
+            if (outFile) {
+              diffs.push(diff);
+            }
+          }
+        }
+
+        if (diagnostics && !filePath.endsWith(".overlay")) {
+          const issues = await fileDiagnosticIssues(connection, f);
+          if (issues?.length) {
+            diagnosticIssues.push({
+              file: f,
+              context,
+              message: issues
+                .map(
+                  (issue) =>
+                    `${issue.message}: {${JSON.stringify(
+                      issue.range.start
+                    )}}-{${JSON.stringify(issue.range.end)}}`
+                )
+                .join("\n\t\t"),
+            });
           }
         }
       })
@@ -178,17 +239,32 @@ async function run(filePaths: string[], logLevel: LogLevel, outFile?: string) {
     fs.writeFileSync(outFile, diffs.join("\n\n"));
   }
 
-  console.log("Processed ", filePaths.length, "files");
+  console.log("Processed", completedPaths.size, "files");
   connection.dispose();
   lspProcess.kill();
 
-  if (error) {
-    console.log(`${error} of ${filePaths.length} Failed`);
-    process.exit(1);
-  } else {
-    console.log("All files passed");
-    process.exit(0);
-  }
+  if (formatingErrors.length)
+    console.log(
+      `${formatingErrors.length} of ${completedPaths.size} Failed formating checks`
+    );
+  else console.log(`All files passed formating`);
+
+  if (diagnosticIssues.length) {
+    console.log("Diagnostic issues summary");
+    console.log(
+      diagnosticIssues
+        .map(
+          (issue) =>
+            `File: ${issue.file}\n\tBoard File: ${issue.context.mainDtsPath.file}\n\tIssues:\n\t\t${issue.message}`
+        )
+        .join("\n")
+    );
+    console.log(
+      `${diagnosticIssues.length} of ${completedPaths.size} Failed diagnostic checks`
+    );
+  } else console.log(`All files passed diagnostic checks`);
+
+  process.exit(formatingErrors.length || diagnosticIssues ? 1 : 0);
 }
 
 const flatFileTree = (file: File): string[] => {
@@ -213,20 +289,56 @@ const formatFile = async (connection: MessageConnection, absPath: string) => {
   )) as string | undefined;
 
   if (diff) {
-    console.error(`❌ ${absPath}`);
+    console.error(`[${i}/${total}] ❌ ${absPath}`);
 
     if (logLevel !== "none") {
       console.log(diff);
     }
     return diff;
   } else {
-    console.log(`✅ ${absPath} is correctly formatted.`);
+    console.log(`[${i}/${total}] ✅ ${absPath} is correctly formatted.`);
+  }
+};
+
+const fileDiagnosticIssues = async (
+  connection: MessageConnection,
+  absPath: string
+) => {
+  const issues = (
+    ((await connection.sendRequest("devicetree/diagnosticIssues", {
+      uri: `file://${absPath}`,
+    })) as Diagnostic[] | undefined) ?? []
+  ).filter(
+    (issue) =>
+      issue.severity === DiagnosticSeverity.Error ||
+      issue.severity === DiagnosticSeverity.Warning
+  );
+
+  if (issues.length) {
+    console.error(
+      `[${i}/${total}] ❌ ${absPath} has ${issues.length} diagnostic errors!`
+    );
+
+    if (logLevel !== "none") {
+      console.log(
+        issues.map(
+          (issue) =>
+            `${issue.message}: ${JSON.stringify(
+              issue.range.start
+            )}${JSON.stringify(issue.range.end)}`
+        )
+      );
+    }
+
+    return issues;
+  } else {
+    console.log(`[${i}/${total}] ✅ ${absPath} has no diagnostic errors.`);
   }
 };
 
 function waitForNewActiveContext(
   connection: MessageConnection,
-  timeoutMs = 5000
+  timeoutMs = 60000
 ): Promise<ContextListItem> {
   return new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
